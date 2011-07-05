ee2487e0817df93e53e7c7d5fff4dd5fddb8a8f7
Davi Ferreira <contato@daviferreira.com>
2011-07-05 11:14:56 -0300

    Moved site dir.

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,4 @@
-# Django settings for programmingquotes project.
+# Django settings for mysite project.
 
 DEBUG = False 
 TEMPLATE_DEBUG = DEBUG
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'programmingquotes.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
