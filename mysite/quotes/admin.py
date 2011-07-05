fda5360140a9267ccfa7ad50bdd0f99acce78686
Davi Ferreira <contato@daviferreira.com>
2011-07-05 15:09:53 -0300

    Admin select ordering.

--- mysite/quotes/admin.py
+++ mysite/quotes/admin.py
@@ -1,6 +1,19 @@
 from quotes.models import Author, Tag, Quote
+from django import forms
 from django.contrib import admin
+
+class QuoteAdminForm(forms.ModelForm):
+    class Meta:
+        model = Quote
+
+    def __init__(self, *args, **kwds):
+        super(QuoteAdminForm, self).__init__(*args, **kwds)
+        self.fields['authors'].queryset = Author.objects.order_by('name')
+        self.fields['tags'].queryset = Tag.objects.order_by('name')
+
+class QuoteAdmin(admin.ModelAdmin):
+     form = QuoteAdminForm
 
 admin.site.register(Author)
 admin.site.register(Tag)
-admin.site.register(Quote)
+admin.site.register(Quote, QuoteAdmin)
