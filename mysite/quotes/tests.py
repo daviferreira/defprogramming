0b33bca8cb0ab725bbf6e60ccbba5308353b2695
Davi Ferreira <contato@daviferreira.com>
2011-07-19 10:16:40 -0300

    Some testing.

--- mysite/quotes/tests.py
+++ mysite/quotes/tests.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.test import TestCase
 from django.test.client import Client
 from quotes.models import Author, Tag, Quote
@@ -6,10 +7,20 @@ class TestQuote(TestCase):
 
   def setUp(self):
     self.client = Client()
-    pass
+    body = "Test quote"
+    now = datetime.now()
+    self.quote = Quote.objects.create(body=body, publish_date=now)
+    self.quote.authors.create(name="Author 1")
+    self.quote.authors.create(name="Author 2")
+    self.quote.tags.create(name="Tag 1")
+    self.quote.tags.create(name="Tag 2")
 
   def tearDown(self):
     pass
+
+  def test_models(self):
+    self.assertEqual(self.quote.authors.all().count(), 2)
+    self.assertEqual(self.quote.tags.all().count(), 2)
 
   def testIndexPage(self):
     response = self.client.get('/')
