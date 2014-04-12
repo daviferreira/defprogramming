b14f920d4e206be9f5b7cd7219ed083a36606793
Davi Ferreira <contato@daviferreira.com>
2014-04-13 07:23:33 +1000

    Flake8

--- defprogramming/quotes/models.py
+++ defprogramming/quotes/models.py
@@ -1,70 +1,79 @@
+# coding: utf-8
 from django.db import models
 from django.core.urlresolvers import reverse
 from datetime import datetime
 
+
 class Author(models.Model):
-  name = models.CharField(max_length=200)
-  short_bio = models.CharField(max_length=200, null=True, blank=True)
-  slug = models.SlugField(max_length=100, blank=True)
-  ordering = ['name']
-  
-  def __unicode__(self):
-    return self.name
-    
-  def get_absolute_url(self):
-    return reverse('quotes.views.author_detail', kwargs={'slug': self.slug})
+    name = models.CharField(max_length=200)
+    short_bio = models.CharField(max_length=200, null=True, blank=True)
+    slug = models.SlugField(max_length=100, blank=True)
+    ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('author', kwargs={'slug': self.slug})
+
 
 class Tag(models.Model):
-  name = models.CharField(max_length=100)
-  slug = models.SlugField(max_length=100, blank=True)
-  ordering = ['name']
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, blank=True)
+    ordering = ['name']
 
-  def __unicode__(self):
-    return self.name
+    def __unicode__(self):
+        return self.name
 
-  def get_absolute_url(self):
-    return reverse('quotes.views.tag_detail', kwargs={'slug': self.slug})
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'slug': self.slug})
+
 
 class Quote(models.Model):
-    body = models.TextField()
-    authors = models.ManyToManyField(Author)
-    tags = models.ManyToManyField(Tag, blank=True, null=True)
-    slug = models.SlugField(max_length=100, blank=True)
-    publish_date = models.DateTimeField(default=datetime.now)
-    source = models.TextField(null=True, blank=True)
-    ordering = ['-publish_date']
-    
-    def __unicode__(self):
-      return self.body
-      
-    def get_absolute_url(self):
-      return reverse('quotes.views.detail', kwargs={'slug': self.slug})
+        body = models.TextField()
+        authors = models.ManyToManyField(Author)
+        tags = models.ManyToManyField(Tag, blank=True, null=True)
+        slug = models.SlugField(max_length=100, blank=True)
+        publish_date = models.DateTimeField(default=datetime.now)
+        source = models.TextField(null=True, blank=True)
+        ordering = ['-publish_date']
 
-    def get_html_title(self):
-      title = "Programming quote by "
-      authors = self.authors.all()
-      last = ""
-      for last in authors:
-        pass
-      for author in authors:
-        title += author.name 
-        if author != last:
-          title += ", "
-      return title
+        def __unicode__(self):
+            return self.body
+
+        def get_absolute_url(self):
+            return reverse('quote', kwargs={'slug': self.slug})
+
+        def get_html_title(self):
+            title = "Programming quote by "
+            authors = self.authors.all()
+            last = ""
+            for last in authors:
+                pass
+            for author in authors:
+                title += author.name
+                if author != last:
+                    title += ", "
+            return title
+
 
 # signals
 from django.db.models import signals
 from django.template.defaultfilters import slugify
 
+
 def author_pre_save(signal, instance, sender, **kwargs):
-  instance.slug = slugify(instance.name[0:99])
-  
+    instance.slug = slugify(instance.name[0:99])
+
+
 def quote_pre_save(signal, instance, sender, **kwargs):
-  instance.slug = slugify(instance.body[0:99])
+    instance.slug = slugify(instance.body[0:99])
+
 
 def tag_pre_save(signal, instance, sender, **kwargs):
-  instance.slug = slugify(instance.name[0:99])
-  
+    instance.slug = slugify(instance.name[0:99])
+
+
 signals.pre_save.connect(author_pre_save, sender=Author)
 signals.pre_save.connect(quote_pre_save, sender=Quote)
 signals.pre_save.connect(tag_pre_save, sender=Tag)
