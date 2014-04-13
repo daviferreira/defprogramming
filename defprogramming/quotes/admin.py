1d4d635e606ebab0b81d3bd2e21272661bd6ec96
Davi Ferreira <contato@daviferreira.com>
2014-04-13 21:01:56 +1000

    Flake8

--- defprogramming/quotes/admin.py
+++ defprogramming/quotes/admin.py
@@ -1,28 +1,32 @@
+# coding: utf-8
 from quotes.models import Author, Tag, Quote
 from django import forms
 from django.contrib import admin
 
-class QuoteAdminForm(forms.ModelForm):
-  class Meta:
-    model = Quote
 
-  def __init__(self, *args, **kwds):
-    super(QuoteAdminForm, self).__init__(*args, **kwds)
-    self.fields['authors'].queryset = Author.objects.order_by('name')
-    self.fields['tags'].queryset = Tag.objects.order_by('name')
+class QuoteAdminForm(forms.ModelForm):
+    class Meta:
+        model = Quote
+
+    def __init__(self, *args, **kwds):
+        super(QuoteAdminForm, self).__init__(*args, **kwds)
+        self.fields['authors'].queryset = Author.objects.order_by('name')
+        self.fields['tags'].queryset = Tag.objects.order_by('name')
+
 
 class QuoteAdmin(admin.ModelAdmin):
-  list_display = ('id', 'body') 
-  list_filter = ('authors', 'tags')
-  search_fields = ('id', 'body')
-  date_hierarchy = 'publish_date'
-  ordering = ('-publish_date',)
-  form = QuoteAdminForm
+    list_display = ('id', 'body')
+    list_filter = ('authors', 'tags')
+    search_fields = ('id', 'body')
+    date_hierarchy = 'publish_date'
+    ordering = ('-publish_date',)
+    form = QuoteAdminForm
 
 
 class AuthorAdmin(admin.ModelAdmin):
-  ordering = ('name',)
+    ordering = ('name',)
+
 
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Tag)
 admin.site.register(Quote, QuoteAdmin)
+admin.site.register(Tag)
