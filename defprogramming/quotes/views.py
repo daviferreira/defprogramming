b6eaed36796fe958cab295e88e10713281cdc2ae
Davi Ferreira <contato@daviferreira.com>
2014-04-13 13:00:34 +1000

    Flake8

--- defprogramming/quotes/views.py
+++ defprogramming/quotes/views.py
@@ -1,77 +1,85 @@
-from django.template import Context, loader, RequestContext
+from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from quotes.models import Author, Tag, Quote
 from quotes.forms import QuoteForm
-from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
 def index(request):
-  featured_quote = Quote.objects.filter(featured=True).order_by('?')[:1][0]
-  quotes = Quote.objects.all().exclude(id=featured_quote.id).order_by('-publish_date')
-  quotes = __validates_pagination(request, Paginator(quotes, 25))
-  title = "def programming: quotes about coding"
-  description = "Quotes about programming, coding, computer science, debugging, software industry, startups and motivation."
-  return render_to_response('quotes/index.html', locals(), context_instance=RequestContext(request))
+    featured_quote = Quote.objects.filter(featured=True).order_by('?')[:1][0]
+    quotes = Quote.objects.all().exclude(id=featured_quote.id).order_by('-publish_date')
+    quotes = __validates_pagination(request, Paginator(quotes, 25))
+    title = "def programming: quotes about coding"
+    description = "Quotes about programming, coding, computer science, debugging, software industry, startups and motivation."
+    return render_to_response('quotes/index.html', locals(), context_instance=RequestContext(request))
+
 
 def detail(request, slug):
-  quote = get_object_or_404(Quote, slug=slug)
-  return render_to_response('quotes/detail.html', locals(), context_instance=RequestContext(request))
+    quote = get_object_or_404(Quote, slug=slug)
+    return render_to_response('quotes/detail.html', locals(), context_instance=RequestContext(request))
+
 
 def random(request):
-  quote = Quote.objects.order_by('?')[0]
-  return render_to_response('quotes/detail.html', locals(), context_instance=RequestContext(request))
+    quote = Quote.objects.order_by('?')[0]
+    return render_to_response('quotes/detail.html', locals(), context_instance=RequestContext(request))
+
 
 def authors(request):
-  authors = Author.objects.all().order_by('name')
-  title = "Listing all authors | def programming"
-  description = "List of all the authors with quotes published. Quotes about programming, coding, software industry.";
-  return render_to_response('quotes/authors.html', locals(), context_instance=RequestContext(request))
+    authors = Author.objects.all().order_by('name')
+    title = "Listing all authors | def programming"
+    description = "List of all the authors with quotes published. Quotes about programming, coding, software industry."
+    return render_to_response('quotes/authors.html', locals(), context_instance=RequestContext(request))
+
 
 def author_detail(request, slug):
-  author = get_object_or_404(Author, slug=slug)
-  quotes = author.quote_set.all().order_by('-publish_date')
-  quotes = __validates_pagination(request, Paginator(quotes, 10))
-  title = "Programming quotes by " + author.name + " | def programming"
-  description = "Listing all programming quotes by " + author.name + ". Quotes about programming, coding, software industry."
-  return render_to_response('quotes/author_detail.html', locals(), context_instance=RequestContext(request))
+    author = get_object_or_404(Author, slug=slug)
+    quotes = author.quote_set.all().order_by('-publish_date')
+    quotes = __validates_pagination(request, Paginator(quotes, 10))
+    title = "Programming quotes by " + author.name + " | def programming"
+    description = "Listing all programming quotes by " + author.name + ". Quotes about programming, coding, software industry."
+    return render_to_response('quotes/author_detail.html', locals(), context_instance=RequestContext(request))
+
 
 def tags(request):
-  tags = Tag.objects.all().order_by('name')
-  title = "Listing all tags | def programming"
-  description = "Tags list. Quotes about programming, coding, software industry.";
-  return render_to_response('quotes/tags.html', locals(), context_instance=RequestContext(request))
+    tags = Tag.objects.all().order_by('name')
+    title = "Listing all tags | def programming"
+    description = "Tags list. Quotes about programming, coding, software industry."
+    return render_to_response('quotes/tags.html', locals(), context_instance=RequestContext(request))
+
 
 def tag_detail(request, slug):
-  tag = get_object_or_404(Tag, slug=slug)
-  quotes = tag.quote_set.all().order_by('-publish_date')
-  quotes = __validates_pagination(request, Paginator(quotes, 10))
-  title = "Programming quotes tagged under " + tag.name + " | def programming"
-  description = "Listing all programming quotes tagged under " + tag.name + ". Quotes about programming, coding, software industry."
+    tag = get_object_or_404(Tag, slug=slug)
+    quotes = tag.quote_set.all().order_by('-publish_date')
+    quotes = __validates_pagination(request, Paginator(quotes, 10))
+    title = "Programming quotes tagged under " + tag.name + " | def programming"
+    description = "Listing all programming quotes tagged under " + tag.name + ". Quotes about programming, coding, software industry."
 
-  return render_to_response('quotes/tag_detail.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('quotes/tag_detail.html', locals(), context_instance=RequestContext(request))
+
 
 def submit_quote(request):
-  title = "Submit a quote | def programming"
-  description = "Use this form to submit a quote. Please send only quotes about programming, coding, software industry."
-  sent = False
+    title = "Submit a quote | def programming"
+    description = "Use this form to submit a quote. Please send only quotes about programming, coding, software industry."
+    sent = False
 
-  if request.method == 'POST':
-    form = QuoteForm(request.POST)
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
 
-    if form.is_valid():
-      form.save(request.POST);
-      sent = True
+        if form.is_valid():
+            form.save(request.POST)
+            sent = True
 
-  else:
-    form = QuoteForm()
+    else:
+        form = QuoteForm()
 
-  return render_to_response('quotes/submit_quote.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('quotes/submit_quote.html', locals(), context_instance=RequestContext(request))
+
 
 def __validates_pagination(request, paginator):
-  page = request.GET.get('page') or 1
-  try:
-    return paginator.page(page)
-  except PageNotAnInteger:
-    return paginator.page(1)
-  except EmptyPage:
-    return paginator.page(paginator.num_pages)
+    page = request.GET.get('page') or 1
+    try:
+        return paginator.page(page)
+    except PageNotAnInteger:
+        return paginator.page(1)
+    except EmptyPage:
+        return paginator.page(paginator.num_pages)
