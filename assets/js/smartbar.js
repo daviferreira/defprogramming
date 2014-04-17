c5237ad4f098cff51eaf1c7fa7ea59f182059262
Davi Ferreira <contato@daviferreira.com>
2014-04-17 13:57:16 +1000

    Implements prototype for navbar behavior

--- assets/js/smartbar.js
+++ assets/js/smartbar.js
@@ -1,14 +1,13 @@
-/*global window*/
-
 var smartBar;
 
-(function () {
+(function (window, document, undefined) {
     'use strict';
 
     smartBar = {
         init: function (el, limit) {
             this.root = el;
             this.limit = limit;
+            this.isVisible = false;
             this.bindScroll();
         },
 
@@ -21,9 +20,21 @@ var smartBar;
                 clearTimeout(timer);
                 timer = setTimeout(function () {
                     scrollDirection = self.getScrollDirection(lastScrollTop);
-                    console.log(scrollDirection);
+                    // refactor plz
+                    if (window.pageYOffset > self.limit) {
+                        self.stick();
+                        if (scrollDirection === 'up' && !self.isVisible) {
+                            self.show();
+                        } else if (scrollDirection === 'down' && self.isVisible) {
+                            self.hide();
+                        }
+                    } else if (window.pageYOffset > self.root.offsetHeight) {
+                        self.stick();
+                    } else {
+                        self.unstick();
+                    }
                     lastScrollTop = window.pageYOffset;
-                }, 100);
+                }, 50);
             });
         },
 
@@ -35,11 +46,31 @@ var smartBar;
         },
 
         show: function show() {
-            this.root.setAttribute('data-visible', true);
+            this.isVisible = true;
+            setTimeout(function () {
+                this.root.setAttribute('data-visible', true);
+            }.bind(this), 200);
         },
 
         hide: function hide() {
+            this.isVisible = false;
+            this.root.setAttribute('data-visible', false);
+        },
+
+        stick: function stick() {
+            if (!this.isSticky) {
+                this.root.setAttribute('data-visible', false);
+                this.root.setAttribute('data-sticky', true);
+                document.body.style.paddingTop = this.root.offsetHeight + 'px';
+                this.isSticky = true;
+            }
+        },
+
+        unstick: function unstick() {
+            document.body.style.paddingTop = '';
+            this.isSticky = false;
+            this.root.setAttribute('data-sticky', false);
             this.root.setAttribute('data-visible', false);
         }
     };
-}());
+}(window, document));
