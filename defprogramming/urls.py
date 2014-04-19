9b0b9f325fbe9ef47e079bbeb633c40a32a7762c
Davi Ferreira <contato@daviferreira.com>
2014-04-19 17:39:22 +1000

    Changes author url, flake8

--- defprogramming/urls.py
+++ defprogramming/urls.py
@@ -1,38 +1,43 @@
+# coding: utf-8
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
 from quotes.models import Quote, Author, Tag
-admin.autodiscover()
-
 from quotes.feeds import LatestEntriesFeed
 
+
+admin.autodiscover()
+
+
 quotes = {
-  'queryset': Quote.objects.all(),
-  'date_field': 'publish_date'
+    'queryset': Quote.objects.all(),
+    'date_field': 'publish_date'
 }
 
 authors = {
-  'queryset': Author.objects.all()
+    'queryset': Author.objects.all()
 }
 
 tags = {
-  'queryset': Tag.objects.all()
+    'queryset': Tag.objects.all()
 }
 
 sitemaps = {
-  'quotes': GenericSitemap(quotes, changefreq = 'weekly', priority=0.6),
-  'authors': GenericSitemap(authors, changefreq = 'weekly', priority=0.6),
-  'tags': GenericSitemap(tags, changefreq = 'weekly', priority=0.6),
+    'quotes': GenericSitemap(quotes, changefreq='weekly', priority=0.6),
+    'authors': GenericSitemap(authors, changefreq='weekly', priority=0.6),
+    'tags': GenericSitemap(tags, changefreq='weekly', priority=0.6),
 }
 
-urlpatterns = patterns('quotes.views',
+urlpatterns = patterns(
+    'quotes.views',
     url(r'^quote/(?P<slug>[\w_-]+)/$', 'detail', name='quote'),
     url(r'^quote/(?P<quote_id>\d+)/$', 'detail'),
-    #TODO: (r'^quote/(?P<quote_uuid>\d+)/$', 'detail'),
+    # TODO: (r'^quote/(?P<quote_uuid>\d+)/$', 'detail'),
     url(r'^authors/$', 'authors', name='authors'),
-    url(r'^author/(?P<slug>[\w_-]+)/$', 'author_detail', name='author'),
+    url(r'^quotes-by/(?P<slug>[\w_-]+)/$', 'author_detail', name='author'),
+    url(r'^author/(?P<slug>[\w_-]+)/$', 'author_detail'),
     url(r'^author/(?P<author_id>\d+)/$', 'author_detail'),
     url(r'^tags/$', 'tags', name='tags'),
     url(r'^tag/(?P<slug>[\w_-]+)/$', 'tag_detail', name='tag'),
@@ -43,13 +48,15 @@ urlpatterns = patterns('quotes.views',
 )
 
 urlpatterns += [
-  url(r'^feed/$', LatestEntriesFeed(), name='feed'),
+    url(r'^feed/$', LatestEntriesFeed(), name='feed'),
 ]
 
-urlpatterns += patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-    (r'^media/(.*)$', 'django.views.static.serve',
+urlpatterns += patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': sitemaps}),
+    url(r'^media/(.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}),
 )
 
