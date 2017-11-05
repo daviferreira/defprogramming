fb5e0f7ab48c49e429487d6455cc4f943e120b5c
Davi Ferreira <contato@daviferreira.com>
2017-11-05 14:55:45 +0100

    Allow http for now

--- defprogramming/settings_production.py
+++ defprogramming/settings_production.py
@@ -1,6 +1,6 @@
 from defprogramming.settings import *
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 # Update database configuration with $DATABASE_URL.
@@ -8,8 +8,8 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-MIDDLEWARE_CLASSES += ('sslify.middleware.SSLifyMiddleware',)
+# MIDDLEWARE_CLASSES += ('sslify.middleware.SSLifyMiddleware',)
 
 PREPEND_WWW = True
