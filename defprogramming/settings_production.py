11808cc9d10d1888febcca40313dea045e85042e
Davi Ferreira <contato@daviferreira.com>
2017-11-05 14:16:22 +0100

    Force https

--- defprogramming/settings_production.py
+++ defprogramming/settings_production.py
@@ -7,3 +7,7 @@ TEMPLATE_DEBUG = DEBUG
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+MIDDLEWARE_CLASSES += ('sslify.middleware.SSLifyMiddleware',)
